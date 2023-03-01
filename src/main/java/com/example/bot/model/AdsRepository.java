5be2e7cc02fe351b0f08bf8e72471524f9459d45
Alester <90714403+TaarMaan@users.noreply.github.com>
2023-03-01 19:33:42 +0300

    fourth init added 2 local db+ timer sendng all mes + refact

---
+++ src/main/java/com/example/bot/model/AdsRepository.java
@@ -0,0 +1,6 @@
+package com.example.bot.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdsRepository extends CrudRepository<Ads, Long> {
+}
--- src/main/java/com/example/bot/service/TelegramBot.java
+++ src/main/java/com/example/bot/service/TelegramBot.java
@@ -1,19 +1,24 @@
 package com.example.bot.service;
 
 import com.example.bot.config.BotConfig;
+import com.example.bot.model.Ads;
 import com.example.bot.model.Repository;
 import com.example.bot.model.User;
 import com.vdurmont.emoji.EmojiParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import com.example.bot.model.AdsRepository;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
-import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.Timestamp;
@@ -26,29 +31,22 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Autowired
     private Repository repository;
+    @Autowired
+    private AdsRepository AdsRepository;
     final BotConfig botConfig;
 
-static final String HELP_TEXT = "This bot is created to demonstrate.\n\n" +
-        "You can eecute commands from the main menu on the left or bytyping a comman:\n\n" +
-        "Type /start to see a welcome message\n\n" +
-        "Type /data to see data stored about yourself\n\n"+
-        "Type /help to see this message again";
+    static final String HELP_TEXT = "This bot is created to demonstrate.\n\n" +
+            "You can eecute commands from the main menu on the left or bytyping a comman:\n\n" +
+            "Type /start to see a welcome message\n\n" +
+            "Type /data to see data stored about yourself\n\n" +
+            "Type /help to see this message again";
+
+    static final String YES_BUTTON = "YES_BUTTON";
+    static final String NO_BUTTON = "NO_BUTTON";
+    static final String ERROR_TEXT = "Error occurred: ";
 
     public TelegramBot(BotConfig botConfig) {
         this.botConfig = botConfig;
-        //Этот способ устарел, меню сделано при помощи функционала @BotFathrer
-        /*List listofCommands = new ArrayList<>();
-        listofCommands.add(new BotCommand("/start","Get a welcome message"));
-        listofCommands.add(new BotCommand("/myData","Get your data stored"));
-        listofCommands.add(new BotCommand("/deleteData","Delete my data"));
-        listofCommands.add(new BotCommand("/help","How to use this bot"));
-        listofCommands.add(new BotCommand("/settings","Set your preferences"));
-        try {
-            this.execute(new SetMyCommands(listofCommands,new BotCommandScopeDefault(),null));
-        }
-        catch (TelegramApiException e){
-            log.error("error setting bot's comman list: " + e.getMessage());
-        }*/
     }
 
     @Override
@@ -56,24 +54,74 @@ static final String HELP_TEXT = "This bot is created to demonstrate.\n\n" +
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-            switch (messageText) {
-                case "/start":
 
-                    registerUser(update.getMessage());
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                case "/help":
-                    sendMessage(chatId, HELP_TEXT);
-                    break;
-                default:
-                    sendMessage(chatId, "sorry, this command is unknown for bot");
-                    break;
+            if (messageText.contains("/send") && botConfig.getOwnerId() == chatId) {
+                var textToSend = EmojiParser.parseToUnicode(messageText.substring(messageText.indexOf(" ")));
+                var users = repository.findAll();
+                for (User user : users) {
+                    prepareAndSendMessage(user.getChatId(), textToSend);
+                }
+            } else {
+
+                switch (messageText) {
+                    case "/start" -> {
+                        registerUser(update.getMessage());
+                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    }
+                    case "/help" -> prepareAndSendMessage(chatId, HELP_TEXT);
+                    case "/register" -> register(chatId);
+                    default -> prepareAndSendMessage(chatId, "Sorry, command was not recognized");
+                }
+            }
+        } else if (update.hasCallbackQuery()) {
+            String callbackData = update.getCallbackQuery().getData();
+            long messageId = update.getCallbackQuery().getMessage().getMessageId();
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+
+            if (callbackData.equals(YES_BUTTON)) {
+                String text = "You pressed YES button";
+                executeEditMessageText(text, chatId, messageId);
+            } else if (callbackData.equals(NO_BUTTON)) {
+                String text = "You pressed NO button";
+                executeEditMessageText(text, chatId, messageId);
             }
         }
+
+
+    }
+
+    private void register(long chatId) {
+
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Do you really want to register?");
+
+        InlineKeyboardMarkup markupInLine = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+        var yesButton = new InlineKeyboardButton();
+
+        yesButton.setText("Yes");
+        yesButton.setCallbackData(YES_BUTTON);
+
+        var noButton = new InlineKeyboardButton();
+
+        noButton.setText("No");
+        noButton.setCallbackData(NO_BUTTON);
+
+        rowInLine.add(yesButton);
+        rowInLine.add(noButton);
+
+        rowsInLine.add(rowInLine);
+
+        markupInLine.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInLine);
+
+        executeMessage(message);
     }
 
     private void registerUser(Message message) {
-        if(repository.findById(message.getChatId()).isEmpty()){
+        if (repository.findById(message.getChatId()).isEmpty()) {
             var chatId = message.getChatId();
             var chat = message.getChat();
 
@@ -90,9 +138,7 @@ static final String HELP_TEXT = "This bot is created to demonstrate.\n\n" +
     }
 
     private void startCommandReceived(long chatId, String name) {
-
-
-        String answer = EmojiParser.parseToUnicode("Hi, " + name + ", nice to meet you"+ ":blush:");
+        String answer = EmojiParser.parseToUnicode("Hi, " + name + ", nice to meet you" + ":blush:");
         log.info("Replied to user " + name);
         sendMessage(chatId, answer);
     }
@@ -101,10 +147,70 @@ static final String HELP_TEXT = "This bot is created to demonstrate.\n\n" +
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
+
+        //реализация клавиатуры для всех сообщений однотипно
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+
+        KeyboardRow keyboardRow = new KeyboardRow();
+
+        keyboardRow.add("weather");
+        keyboardRow.add("get random joke");
+
+        keyboardRows.add(keyboardRow);
+
+        keyboardRow = new KeyboardRow();
+
+        keyboardRow.add("/register");
+        keyboardRow.add("check my data");
+        keyboardRow.add("delete my data");
+
+        keyboardRows.add(keyboardRow);
+
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+        message.setReplyMarkup(replyKeyboardMarkup);
+        executeMessage(message);
+    }
+
+    private void executeEditMessageText(String text, long chatId, long messageId) {
+        EditMessageText message = new EditMessageText();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+        message.setMessageId((int) messageId);
+
         try {
             execute(message);
         } catch (TelegramApiException e) {
-            log.error("Error occurred: " + e.getMessage());
+            log.error(ERROR_TEXT + e.getMessage());
+        }
+    }
+
+    private void executeMessage(SendMessage message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error(ERROR_TEXT + e.getMessage());
+        }
+    }
+
+    private void prepareAndSendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        executeMessage(message);
+    }
+
+    @Scheduled(cron = "${cron.scheduler}")
+    private void sendAds() {
+
+        var ads = AdsRepository.findAll();
+        var users = repository.findAll();
+
+        for (Ads ad : ads) {
+            for (User user : users) {
+                prepareAndSendMessage(user.getChatId(), ad.getAd());
+            }
         }
     }
 
